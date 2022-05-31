73ec638452a64ef5f35cff9e419db2619779deed
MetatransApps <kitopchiyski@gmail.com>
2022-05-31 13:31:22 +0300

    Sound menu and background color fix

--- app/src/main/java/org/metatrans/apps/stroop/menu/Activity_Menu_Main.java
+++ app/src/main/java/org/metatrans/apps/stroop/menu/Activity_Menu_Main.java
@@ -9,17 +9,14 @@ import org.metatrans.apps.stroop.lib.R;
 import org.metatrans.commons.app.Application_Base;
 import org.metatrans.commons.cfg.menu.Config_MenuMain_Base;
 import org.metatrans.commons.cfg.menu.IConfigurationMenu_Main;
-import org.metatrans.commons.menu.Activity_Menu_Main_Base;
 
 import android.content.Intent;
 
 
-public class Activity_Menu_Main extends Activity_Menu_Main_Base {
+public class Activity_Menu_Main extends org.metatrans.commons.questionnaire.menu.Activity_Menu_Main {
 
 
 	public static int CFG_MENU_MODES			 		= 15;
-	public static int CFG_MENU_RESULT			 		= 16;
-	public static int CFG_MENU_ACHIEVEMENTS		 		= 17;
 	
 	
 	@Override
@@ -63,84 +60,6 @@ public class Activity_Menu_Main extends Activity_Menu_Main_Base {
 						startActivity(i);
 						finish();
 						
-					}
-				};
-			}
-		});
-
-
-		result.add(new Config_MenuMain_Base() {
-
-			@Override
-			public int getName() {
-				return R.string.results;
-			}
-
-			@Override
-			public int getIconResID() {
-				return R.drawable.ic_123;
-			}
-
-			@Override
-			public int getID() {
-				return CFG_MENU_RESULT;
-			}
-
-			@Override
-			public String getDescription_String() {
-				return "";
-			}
-
-			@Override
-			public Runnable getAction() {
-
-				return new Runnable() {
-
-					@Override
-					public void run() {
-
-						int modeID = Application_Base.getInstance().getUserSettings().modeID;
-
-						Application_Base.getInstance().getEngagementProvider().getLeaderboardsProvider().openLeaderboard_LocalOnly(modeID);
-
-						Application_Base.getInstance().getEngagementProvider().getLeaderboardsProvider().openLeaderboard(modeID);
-					}
-				};
-			}
-		});
-
-
-		result.add(new Config_MenuMain_Base() {
-
-			@Override
-			public int getName() {
-				return R.string.achievements;
-			}
-
-			@Override
-			public int getIconResID() {
-				return org.metatrans.commons.R.drawable.ic_cup;
-			}
-
-			@Override
-			public int getID() {
-				return Activity_Menu_Main.CFG_MENU_ACHIEVEMENTS;
-			}
-
-			@Override
-			public String getDescription_String() {
-				return "";
-			}
-
-			@Override
-			public Runnable getAction() {
-
-				return new Runnable() {
-
-					@Override
-					public void run() {
-
-						Application_Base.getInstance().getEngagementProvider().getAchievementsProvider().openAchievements();
 					}
 				};
 			}
